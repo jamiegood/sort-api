92730c61b3fcb166cde2ef8e098cbda205612f75
Jamie Good <jamie.good@hostelworld.com>
2019-12-13 16:33:34 +0000

    Added an basic endpoint

--- main.go
+++ main.go
@@ -1,17 +1,48 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
+// Sort ...
+type Sort struct {
+	ID    int    `json:"id"`
+	Title string `json:"title"`
+	Body  string `json:"body"`
+}
+
+var sorts []Sort
+
+func getSortsHandler(w http.ResponseWriter, r *http.Request) {
+
+	sort := Sort{ID: 1, Title: "hoho", Body: "Yup body here"}
+
+	sorts = append(sorts, sort)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(sorts)
+}
+
+// func getSort(w http.ResponseWriter, r *http.Request) {
+// 	w.Header().Set("Content-Type", "application/json")
+// 	params := mux.Vars(r)
+// 	// for _, item := range sorts {
+// 	// 	// if item.ID == params["id"] {
+// 	// 	// 	json.NewEncoder(w).Encode(item)
+// 	// 	// 	break
+// 	// 	// }
+// 	// 	// return
+// 	// }
+// 	json.NewEncoder(w).Encode(&Sort{})
+// }
 func main() {
 	fmt.Println("so far so good")
 
 	router := mux.NewRouter()
-	router.HandleFunc("/sorts/", getSorts).Methods("GET")
+	router.HandleFunc("/sorts/", getSortsHandler).Methods("GET")
 
 	http.ListenAndServe(":8001", router)
 
